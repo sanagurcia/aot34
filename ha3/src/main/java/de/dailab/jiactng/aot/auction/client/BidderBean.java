f440c2030899a7ab4776756fefaa7f5301bff3f2
hsams <hellen.bey@googlemail.com>
2021-06-25 22:42:40 +0200

    Funktionalität ist eingebaut, allerdings kann ichs noch nicht testen, da er keine messages bekommt...

--- ha3/src/main/java/de/dailab/jiactng/aot/auction/client/BidderBean.java
+++ ha3/src/main/java/de/dailab/jiactng/aot/auction/client/BidderBean.java
@@ -2,8 +2,7 @@ package de.dailab.jiactng.aot.auction.client;
 
 import de.dailab.jiactng.agentcore.AbstractAgentBean;
 import de.dailab.jiactng.agentcore.action.Action;
-import de.dailab.jiactng.agentcore.comm.ICommunicationAddress;
-import de.dailab.jiactng.agentcore.comm.ICommunicationBean;
+import de.dailab.jiactng.agentcore.comm.*;
 import de.dailab.jiactng.agentcore.comm.message.JiacMessage;
 import de.dailab.jiactng.agentcore.knowledge.IFact;
 import de.dailab.jiactng.aot.auction.onto.*;
@@ -12,6 +11,11 @@ import org.sercho.masp.space.event.SpaceObserver;
 import org.sercho.masp.space.event.WriteCallEvent;
 
 import java.io.Serializable;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.util.HashMap;
+import java.util.Map;
 
 /* START */
 // receive startAuctions message from meta bean
@@ -46,8 +50,10 @@ public class BidderBean extends AbstractAgentBean {
 	// the id of the current auction
 	private int auctionsId;
 
-	// the address of the AuctioneerMetaBean
-	private ICommunicationAddress metaBeanAddress;
+
+	private ICommunicationAddress auctioneerAAddress;
+	private ICommunicationAddress auctioneerBAddress;
+	private ICommunicationAddress auctioneerCAddress;
 
 	// our bidder id
 	private String myId;
@@ -60,52 +66,78 @@ public class BidderBean extends AbstractAgentBean {
 	private int auctioneerBId;
 	private int auctioneerCId;
 
+	private Wallet myWallet;
+
+	Map<Integer, Double> bidOnItems;
+
+	// this is the money we would have, if we would buy everything we want
+	private double calculatedMoney;
+
+
+
+
+
+
+
+
 	@Override
 	public void doStart() throws Exception {
 		super.doStart();
-		this.groupAddress = ICommunicationBean.ACTION_JOIN_GROUP;
-		this.auctionsId = -1;
-		this.metaBeanAddress = null;
+
+		//String messageGroup = ICommunicationBean.ACTION_JOIN_GROUP;
+		//IGroupAddress groupAddress = CommunicationAddressFactory.createGroupAddress(messageGroup);
+		//thisAgent.getCommunication().joinGroup(groupAddress);
+
+		this.bidOnItems = new HashMap<>();
 		memory.attach(new BidderBean.MessageObserver(), new JiacMessage());
-		log.info("starting bidder agent");
+		log.info("Starting BidderBean.");
 	}
 
 	@Override
 	public void execute() {
 
+		System.out.println("ICH MACHE ETWAS");
+
+		// because message observer does not seem to work..
+		for (JiacMessage message : memory.removeAll(new JiacMessage())) {
+			handleMessage(message);
+		}
 	}
 
 	private void handleMessage(JiacMessage message) {
 		Object payload = message.getPayload();
 
-		if (payload instanceof StartAuctions) {
+		if (payload instanceof StartAuctions)
+		{
 			this.handleStartAuctions((StartAuctions) payload, message.getSender());
 		}
-		else if (payload instanceof InitializeBidder) {
+		else if (payload instanceof InitializeBidder)
+		{
 			this.handleInitializeBidder((InitializeBidder) payload);
 		}
-		else if (payload instanceof StartAuction) {
-			this.handleStartAuction((StartAuction) payload);
+		else if (payload instanceof StartAuction)
+		{
+			this.handleStartAuction((StartAuction) payload, message.getSender());
 		}
-		else if (payload instanceof CallForBids) {
+		else if (payload instanceof CallForBids)
+		{
 			this.handleCallForBids((CallForBids) payload);
 		}
-		else if (payload instanceof InformBuy) {
+		else if (payload instanceof InformBuy)
+		{
 			this.handleInformBuy((InformBuy) payload);
 		}
-		else if (payload instanceof InformSell) {
+		else if (payload instanceof InformSell)
+		{
 			this.handleInformSell((InformSell) payload);
 		}
-
-		/* TODO: till here */
-
-		else if (payload instanceof EndAuction) {
+		else if (payload instanceof EndAuction)
+		{
 			this.handleEndAuction((EndAuction) payload);
 		}
 	}
 
-	/* receive StartAuctions message from AuctioneerMetaBean
-	 * => reply with Register message to AuctioneerMetaBean */
+	/* receive StartAuctions message from AuctioneerMetaBean => reply with Register message to AuctioneerMetaBean */
 	private void handleStartAuctions(StartAuctions payload, ICommunicationAddress sender){
 		System.out.println(payload.toString());
 
@@ -113,77 +145,166 @@ public class BidderBean extends AbstractAgentBean {
 		if(this.auctionsId == payload.getAuctionsId()) return;
 
 		this.auctionsId = payload.getAuctionsId();
-		this.metaBeanAddress = sender;
 
 		// send Register message to AuctioneerMetaBean
-		Register reg = new Register(this.myId, this.myGroupToken);
-		sendMessage(this.metaBeanAddress, reg);
+		sendMessage(sender, new Register(this.myId, this.myGroupToken));
 	}
 
 	/* receive InitializeBidder message from AuctioneerMetaBean with wallet */
 	private void handleInitializeBidder(InitializeBidder payload){
+		System.out.println(payload.toString());
+
 		// make sure this message is for us
-		if(payload.getBidderId() != this.myId) return;
+		if(!payload.getBidderId().equals(this.myId)) return;
 
+		this.myWallet = payload.getWallet();
+		this.calculatedMoney = this.myWallet.getCredits();
 	}
 
-	private void handleStartAuction(StartAuction payload){
-		// receive startAuction message from each auctioneer bean
-		// => do NOT reply
+	/* receive StartAuction message from each AuctioneerBean (A/B/C) */
+	private void handleStartAuction(StartAuction payload, ICommunicationAddress sender){
+		System.out.println(payload.toString());
+
+		if(payload.getMode() == StartAuction.Mode.A)
+		{
+			this.auctioneerAId = payload.getAuctioneerId();
+			this.auctioneerAAddress = sender;
+		}
+		else if(payload.getMode() == StartAuction.Mode.B)
+		{
+			this.auctioneerBId = payload.getAuctioneerId();
+			this.auctioneerBAddress = sender;
+		}
+		else if(payload.getMode() == StartAuction.Mode.C)
+		{
+			this.auctioneerCId = payload.getAuctioneerId();
+			this.auctioneerCAddress = sender;
+		}
+		else System.out.println("Unknown StartAuction Mode.");
+
+		// TODO: this message may contain number of items and items to be sold...
 	}
 
-	/* Choose Auction Type Handler (A/B/C) on instanceof CallForBids messsage */
+	/* Choose Auction Type Handler (A/B/C) on instanceof CallForBids message */
 	private void handleCallForBids(CallForBids payload){
-		if (payload.getMode() == CallForBids.CfBMode.BUY){
+		System.out.println(payload.toString());
+
+		if (payload.getMode() == CallForBids.CfBMode.BUY)
+		{
 			buyCallForBids(payload);
 		}
-		else if (payload.getMode() == CallForBids.CfBMode.SELL){
+		else if (payload.getMode() == CallForBids.CfBMode.SELL)
+		{
 			sellCallForBids(payload);
 		}
-		else {
-			System.out.println("Unknown CfBMode");
-		}
+		else System.out.println("Unknown CallForBids Mode.");
 	}
 
-	/* React to CfB.SELL */
-	private void sellCallForBids(CallForBids payload) {
-	}
-
-	/* React to CfB.BUY */
+	/* React to CallForBids.BUY - only reply if interested */
 	private void buyCallForBids(CallForBids payload) {
+		SmartGreedy strategy = new SmartGreedy(this.myWallet);
+		double ourOffer = strategy.calculateBid(payload);
+
+		// we are not interested
+		if(ourOffer == -1) return;
+
+		this.calculatedMoney -= ourOffer;
+
+		// if we would not have any money after buying we are not interested
+		if(this.calculatedMoney < 0)
+		{
+			this.calculatedMoney += ourOffer;
+			return;
+		}
+
+		// we send the bid to the auctioneer
+		Bid ourBid = new Bid(payload.getAuctioneerId(), this.myId, payload.getCallId(), ourOffer);
+		if(payload.getAuctioneerId() == this.auctioneerAId) sendMessage(this.auctioneerAAddress, ourBid);
+		else if(payload.getAuctioneerId() == this.auctioneerBId) sendMessage(this.auctioneerBAddress, ourBid);
+		else if(payload.getAuctioneerId() == this.auctioneerCId) sendMessage(this.auctioneerCAddress, ourBid);
+
+		// this means something went wrong, so act like nothing was done
+		else
+		{
+			this.calculatedMoney += ourOffer;
+			return;
+		}
+
+		// add this callId to our bid on items, because InformBuy does not tell you how much money you bid
+		this.bidOnItems.put(payload.getCallId(), ourOffer);
+	}
+
+	/* React to CallForBids.SELL - only reply if interested */
+	private void sellCallForBids(CallForBids payload) {
+		// TODO: enter real function here
+		SmartGreedy strategy = new SmartGreedy(this.myWallet);
+		boolean weWantToSell = strategy.calculateBidDummy(payload);
+
+		// we are not interested
+		if(!weWantToSell) return;
+
+		// we send the bid to the auctioneer
+		Bid ourBid = new Bid(payload.getAuctioneerId(), this.myId, payload.getCallId(), payload.getMinOffer());
+		if(payload.getAuctioneerId() == this.auctioneerAId) sendMessage(this.auctioneerAAddress, ourBid);
+		else if(payload.getAuctioneerId() == this.auctioneerBId) sendMessage(this.auctioneerBAddress, ourBid);
+		else if(payload.getAuctioneerId() == this.auctioneerCId) sendMessage(this.auctioneerCAddress, ourBid);
+		else return;
+
+		// remove sold resources from wallet
+		this.myWallet.remove(payload.getBundle());
 	}
 
 	/* Update state info based on result of Buy (Auction A/C) */
 	private void handleInformBuy(InformBuy payload){
-		// update state info
-		// receive InformBuy message from auctioneer A (success or fail)
+		System.out.println(payload.toString());
+
+		// if we did not buy anything
+		if(payload.getType() == InformBuy.BuyType.INVALID || payload.getType() == InformBuy.BuyType.LOST) {
+			this.calculatedMoney += this.bidOnItems.get(payload.getCallId());
+			this.bidOnItems.remove(payload.getCallId());
+			return;
+		}
+
+		// update the amount of money we have in our wallet
+		this.myWallet.updateCredits(payload.getPrice() * -1);
+
+		// add bought bundle to our wallet
+		this.myWallet.add(payload.getBundle());
+
+		this.bidOnItems.remove(payload.getCallId());
 	}
 
 	/* Update state info based on result of Buy (Auction B/C) */
 	private void handleInformSell(InformSell payload){
-		// update state
-		// receive InformSell message from auctioneer B
+		System.out.println(payload.toString());
+
+		// if we did not sell anything
+		if(payload.getType() == InformSell.SellType.INVALID || payload.getType() == InformSell.SellType.NOT_SOLD) {
+			this.myWallet.add(payload.getBundle());
+			return;
+		}
+
+		// update the amount of money we have in our wallet
+		this.myWallet.updateCredits(payload.getPrice());
 	}
 
+	/* receive EndAuction message from AuctioneerMetaBean */
 	private void handleEndAuction(EndAuction payload){
-		// receive endAuction message from meta bean
+		System.out.println(payload.toString());
 	}
 
-	/* Setter Methods for Spring config in bidder.xml */
-	/* Bidder ID Setter */
-	public void setBidderId(String id) {
-		this.myId = id;
-	}
+	/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+	/*++++++++++++++++++++HELPFUL+STUFF++++++++++++++++++++++++++++*/
+	/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 
-	/* Group ID Setter */
-	public void setGroupToken(String id) {
-		this.myGroupToken = id;
-	}
+	/* Bidder ID Setter for Spring config in bidder.xml */
+	public void setBidderId(String id) { this.myId = id; }
 
-	/* Message Group Setter */
-	public void setMessageGroup(String id) {
-		this.groupAddress = id;
-	}
+	/* Group ID Setter for Spring config in bidder.xml */
+	public void setGroupToken(String id) { this.myGroupToken = id; }
+
+	/* Message Group Setter for Spring config in bidder.xml */
+	public void setMessageGroup(String id) { this.groupAddress = id; }
 
 	/* send message */
 	private void sendMessage(ICommunicationAddress receiver, IFact payload) {
--- ha3/src/main/java/de/dailab/jiactng/aot/auction/client/SmartGreedy.java
+++ ha3/src/main/java/de/dailab/jiactng/aot/auction/client/SmartGreedy.java
@@ -1,8 +1,10 @@
 package de.dailab.jiactng.aot.auction.client;
 import de.dailab.jiactng.aot.auction.onto.*;
 
-import java.util.*;
-import java.util.Map.Entry;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 /*
@@ -11,55 +13,39 @@ import java.util.Map.Entry;
 * returns Double else -1
 */
 public class SmartGreedy {
-    private final Wallet myWallet;
-    private final Map<Resource[], Integer> bundlesMap;
+    private Wallet myWallet;
+    private Map<Resource[], Integer> bundlePrices;
 
     public SmartGreedy(Wallet wallet) {
         this.myWallet = wallet;
-        this.bundlesMap = initBundles();
+        this.initBundles();
     }
 
-    // returns minOffer price, for now
     public double calculateBid(CallForBids cfb) {
         List<Resource> bundle = cfb.getBundle();
         double minOffer = cfb.getMinOffer();
         double brutto = this.calculateBrutto(bundle);
-        if (brutto > minOffer) {
-            return minOffer;
-        } else {
-            return -1;
-        }
+        return 500;
     }
 
     // Calculate added value to wallet from bundle
     private double calculateBrutto(List<Resource> bundle) {
         double walletBefore = this.calculateWalletValue(this.myWallet);
-        this.myWallet.add(bundle);
-        double walletAfter = this.calculateWalletValue(this.myWallet);
-        return walletAfter - walletBefore;
+        // Wallet dummyWallet = this.myWallet.clone();
+        // double walletAfter = this.calculateWalletValue(dummyWallet);
+        // return walletAfter - walletBefore;
+        return 150;
     }
 
-    // while wallet resources not empty, remove bundle and aggregate price
-    private double calculateWalletValue(Wallet wallet)  {
-        // Note: operations on wallet are pass-by-value
-        int totalValue = 0;
-
-        // iterate thru bundles in BundlesMap
-        for(Resource[] bundle: this.bundlesMap.keySet()){
-            System.out.println(Arrays.toString(bundle));    // for testing order
-
-            List<Resource> listBundle = Arrays.asList(bundle);
-            // if wallet contains bundle, add price to total value & remove bundle from wallet
-            if (wallet.contains(listBundle)) {
-                totalValue += this.bundlesMap.get(bundle);
-                wallet.remove(listBundle);
-            }
-        }
-        return (double) totalValue;
+    private double calculateWalletValue(Wallet wallet) {
+        return 150;
     }
 
-    // initialize BundlesMap, sorted in descending price order
-    private Map<Resource[], Integer> initBundles() {
+    private double calculateProfit(List<Resource> bundle) {
+        return 150;
+    }
+
+    private void initBundles() {
         Resource[][] bundles = {{Resource.A, Resource.A}, {Resource.A, Resource.A, Resource.A},
                 {Resource.A, Resource.A, Resource.A, Resource.A}, {Resource.A, Resource.A, Resource.B},
                 {Resource.A, Resource.J, Resource.K}, {Resource.B, Resource.B},
@@ -73,30 +59,15 @@ public class SmartGreedy {
                 {Resource.F, Resource.F}, {Resource.F, Resource.J, Resource.K},
                 {Resource.A, Resource.B, Resource.C, Resource.D, Resource.E, Resource.F, Resource.J, Resource.K}};
         Integer[] prices = {200, 300, 400, 200, 200, 50, 1200, 800, 600, 1600, 800, 400, 200, 100, 300, 1400};
-        Map<Resource[], Integer> unsortedBundles = new HashMap<>();
-        // add <bundle,price> to map, random order
+        this.bundlePrices = new HashMap<>();
         for (int i=0; i<bundles.length; i++){
-            unsortedBundles.put(bundles[i], prices[i]);
+            this.bundlePrices.put(bundles[i], prices[i]);
         }
-        // sort bundles map by price, descending
-        return sortByValue(unsortedBundles);
     }
 
-    // sort Map by price in descending order
-    private static Map<Resource[], Integer> sortByValue(Map<Resource[], Integer> map){
-        // get entry set
-        List<Entry<Resource[], Integer>> list = new ArrayList<>(map.entrySet());
-
-        // sort entry set by value, reverse order
-        list.sort(Entry.comparingByValue(Comparator.reverseOrder()));
-
-        // LinkedHashMap preserves iteration order based on insertion order!
-        Map<Resource[], Integer> result = new LinkedHashMap<>();
-
-        // put entries in new map, in sorted list order
-        for (Entry<Resource[], Integer> entry : list) {
-            result.put(entry.getKey(), entry.getValue());
-        }
-        return result;
+    // FOR TESTING PURPOSES
+    public boolean calculateBidDummy(CallForBids cfb) {
+        return true;
     }
+
 }
